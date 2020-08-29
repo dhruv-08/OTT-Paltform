bc518b9006c63d19e5d26a9805b7f03f85ecbaf7
dhruv-08 <dgarg.cse18@chitkarauniversity.edu.in>
2020-08-29 17:30:50 +0530

    find

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,11 +9,38 @@ import GradeIcon from '@material-ui/icons/Grade';
 import Nav from './Nav'
 import "../search.css";
 import axios from '../Axios/axios'
-import { Grid } from '@material-ui/core';
+import { Dialog, Grid } from '@material-ui/core';
 import DoneIcon from '@material-ui/icons/Done';
 import CloseIcon from '@material-ui/icons/Close';
+import movieTrailer from 'movie-trailer';
+import Data from './Data';
+import ReactPlayer from 'react-player';
 function Search() {
-    const [movies, setmovies] = useState([])
+    const [movies, setmovies] = useState([]);
+    const [bool, setbool] = useState([]);
+    const [open, setOpen] =useState(false);
+    const [trailer, settrailer] = useState("");
+    const [value, setvalue] = useState("");
+    const [search, setsearch] = useState([])
+    const handleClose = () => {
+        setOpen(false);
+        settrailer("");
+    };
+    function handleModal(movie){
+        setOpen(true);
+        movieTrailer(movie?.name || movie?.title || movie?.original_name)
+        .then((url)=>{
+            settrailer(url);
+        })
+        .catch((err)=>{
+            console.log("Done");
+        })
+        setbool(movie);
+    }
+    async function handlesearch(){
+        const vall=await axios.get((request.search+"&query="+value));
+        console.log(vall);
+    }
     useEffect(() => {
         async function Data(){
             const val=await axios.get(request.Action);
@@ -29,8 +56,12 @@ function Search() {
             </Helmet>
             <div style={{paddingTop:"80px"}}>
             <SearchBar
-        onChange={() => console.log('onChange')}
-        onRequestSearch={() => console.log('onRequestSearch')}
+        onChange={(v) =>setvalue(v)}
+        onRequestSearch={() =>
+            handlesearch()
+            // console.log(axios.get(request.search+"&query="+value))
+            // setsearch(axios.get(request.search+"&query="+value))
+        }
         style={{
             margin: '0 auto',
             maxWidth: 1490
@@ -40,7 +71,7 @@ function Search() {
        <Grid container className="pos" style={{paddingTop:"2%"}}>
        <FlipMove>
                     {movies.map(movie=>
-                        <Grid key={movie.id} item xs={4} style={{paddingBottom:"2%"}}>
+                        <Grid key={movie.id} item xs={4} style={{paddingBottom:"2%"}} onClick={()=>handleModal(movie)}>
                         <div className="poster" style={{width:"480px",height:"300px",borderRadius:"15px",margin:"auto",boxShadow: "inset 200px 100px 550px #111",backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`}}>
                         <div style={{paddingLeft:"2%",paddingTop:"4%"}}>
                             <span style={{color:"white",paddingBottom:"4%",fontWeight:"lighter",paddingTop:"2%",fontSize:"30px"}}>{movie?.title || movie?.name || movie?.original_name}</span>
@@ -55,6 +86,31 @@ function Search() {
                     </FlipMove>
             </Grid>
             </div>
+            <Dialog fullScreen open={open} onClose={handleClose}>
+                     <Nav/>
+                <Data movie={bool}/>
+                 <div className="main">
+                     <Grid container>
+                         {/* <Grid item xs={1}></Grid> */}
+                         <Grid item xs={6}>
+                             <ReactPlayer controls={true} light={true} url={trailer} className="player"/>
+                         </Grid>
+                         <Grid item xs={6} style={{paddingTop:"14%"}}>
+                             <Grid container>
+                             <Grid item xs={4}>
+                             <h1 style={{color:"white"}}><StarsIcon/><span> {bool.vote_average}(Rating)</span></h1>
+                             </Grid>
+                             <Grid item xs={4}>
+                                 <h1 style={{color:"white"}}><CalendarTodayIcon/><span> {bool.release_date}</span></h1>
+                             </Grid>
+                             <Grid item xs={4}>
+                             <h1 style={{color:"white"}}>{bool.adult===true?"A":"U/A"}</h1>
+                             </Grid>
+                             </Grid>
+                         </Grid>
+                     </Grid>
+                     </div>
+             </Dialog>
         </div>
     )
 }
